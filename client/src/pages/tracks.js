8ddd7362bc82daca074b9319f90ee64d7481c8c3
Juan Carlos Quintero <jcquintero.ce@gmail.com>
2021-03-12 12:46:08 +0100

    Add client query

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,5 +1,22 @@
 import React from 'react';
+import { gql } from '@apollo/client';
 import { Layout } from '../components';
+
+export const TRACKS = gql`
+  query getTracks {
+    tracksForHome {
+      id
+      title
+      thumbnail
+      length
+      modulesCount
+      author {
+        name
+        photo
+      }
+    }
+  }
+`;
 
 /**
  * Tracks Page is the Catstronauts home page.

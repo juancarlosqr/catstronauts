28252a014ab05496bcbbbd5d0d8e227706d5610a
Juan Carlos Quintero <jcquintero.ce@gmail.com>
2021-05-14 17:10:27 +0200

    Add fetch tracks with author test

--- server/src/__tests__/trackAPI.test.js
+++ server/src/__tests__/trackAPI.test.js
@@ -7,11 +7,13 @@ const TrackAPI = require('../datasources/track-api');
 
 casual.seed(4242);
 
+const authorId = casual.uuid;
+
 const mockTracksResponse = [
   {
     id: casual.uuid,
     title: casual.sentence,
-    authorId: casual.uuid,
+    authorId: authorId,
     thumbnail: casual.url,
     length: casual.integer(1, 12),
     modulesCount: casual.integer(1, 5),
@@ -19,24 +21,18 @@ const mockTracksResponse = [
   {
     id: casual.uuid,
     title: casual.sentence,
-    authorId: casual.uuid,
+    authorId: authorId,
     thumbnail: casual.url,
     length: casual.integer(1, 12),
     modulesCount: casual.integer(1, 5),
   },
 ];
 
-const GET_LAUNCHES = gql`
-  query getTracks {
-    tracksForHome {
-      id
-      title
-      length
-      thumbnail
-      modulesCount
-    }
-  }
-`;
+const mockAuthorResponse = {
+  id: authorId,
+  name: casual.name,
+  photo: casual.url,
+};
 
 test('fetches tracks', async () => {
   const trackAPI = new TrackAPI();
@@ -52,10 +48,64 @@ test('fetches tracks', async () => {
   // mock the dataSource's underlying fetch methods
   trackAPI.getTracks = jest.fn(() => mockTracksResponse);
 
+  // create query
+  const GET_LAUNCHES = gql`
+    query getTracks {
+      tracksForHome {
+        id
+        title
+        length
+        thumbnail
+        modulesCount
+      }
+    }
+  `;
+
   // use the test server to create a query function
   const { query } = createTestClient(server);
 
   // run query against the server and snapshot the output
   const res = await query({ query: GET_LAUNCHES });
+  expect(res).toMatchSnapshot();
+});
+
+test('fetches tracks with author', async () => {
+  const trackAPI = new TrackAPI();
+
+  // create a test server to test against,
+  // using our production typeDefs, resolvers, and dataSources
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    dataSources: () => ({ trackAPI }),
+  });
+
+  // mock the dataSource's underlying fetch methods
+  trackAPI.getTracks = jest.fn(() => mockTracksResponse);
+  trackAPI.getAuthor = jest.fn(() => mockAuthorResponse);
+
+  // create query
+  const GET_LAUNCHES_AUTHOR = gql`
+    query getTracksWithAuthor {
+      tracksForHome {
+        id
+        title
+        length
+        thumbnail
+        modulesCount
+        author {
+          id
+          name
+          photo
+        }
+      }
+    }
+  `;
+
+  // use the test server to create a query function
+  const { query } = createTestClient(server);
+
+  // run query against the server and snapshot the output
+  const res = await query({ query: GET_LAUNCHES_AUTHOR });
   expect(res).toMatchSnapshot();
 });

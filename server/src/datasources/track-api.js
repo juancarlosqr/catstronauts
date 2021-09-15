a282e91298e1bb7a9ccd1712d5b9cf5d1572787b
Juan Carlos Quintero <jcquintero.ce@gmail.com>
2021-09-15 10:36:31 +0200

    Add increment views call to datasource

--- server/src/datasources/track-api.js
+++ server/src/datasources/track-api.js
@@ -21,6 +21,10 @@ class TrackAPI extends RESTDataSource {
   getAuthor(authorId) {
     return this.get(`author/${authorId}`);
   }
+
+  incrementTrackViews(trackId) {
+    return this.patch(`track/${trackId}/numberOfViews`);
+  }
 }
 
 module.exports = TrackAPI;

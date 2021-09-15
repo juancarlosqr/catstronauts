7cf237dd4d72d46176a7e6d3bdeff921f4350c75
Juan Carlos Quintero <jcquintero.ce@gmail.com>
2021-09-15 10:53:33 +0200

    Add mutation resolver

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -6,6 +6,18 @@ const resolvers = {
     track: (_parent, { id }, { dataSources }) =>
       dataSources.trackAPI.getTrack(id),
   },
+  Mutation: {
+    incrementTrackViews: async (_, { id }, { dataSources }) => {
+      const track = await dataSources.trackAPI.incrementTrackViews(id);
+
+      return {
+        code: 200,
+        success: true,
+        message: `Successfully incremented number of views for track ${id}`,
+        track,
+      };
+    },
+  },
   Track: {
     author: ({ authorId }, _args, { dataSources }) =>
       dataSources.trackAPI.getAuthor(authorId),

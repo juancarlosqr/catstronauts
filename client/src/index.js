cf59e5fe231ebf69514bb2f2c95d6908fa764f58
Juan Carlos Quintero <jcquintero.ce@gmail.com>
2021-09-15 13:27:01 +0200

    Fix api env var

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import GlobalStyles from './styles';
 import Pages from './pages';
 
 const client = new ApolloClient({
-  uri: process.env.GRAPHQL_API_URI || 'http://localhost:4000',
+  uri: process.env.REACT_APP_GRAPHQL_API_URI || 'http://localhost:4000',
   cache: new InMemoryCache(),
 });
 
